056f1ebfd6856baf94265102cb94c41001086a9c
unknown <danielalfatha@gmail.com>
2026-04-24 16:16:46 +0700

    responsi-modul-4

--- src/routes/loanRoutes.js
+++ src/routes/loanRoutes.js
@@ -4,9 +4,12 @@ import { LoanController } from '../controllers/loanController.js';
 const router = express.Router();
 
 router.get('/', LoanController.getLoans);
+
+router.get('/top-borrowers', LoanController.getTopBorrowers);
+
 router.get('/:id', LoanController.getLoanById);
+
 router.post('/', LoanController.createLoan);
 router.post('/:id/return', LoanController.returnBook);
-router.get('/top-borrowers', LoanController.getTopBorrowers);
 
 export default router;
